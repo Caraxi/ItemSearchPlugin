c25db82154ec90ed315ffbcb63d6515aba094d6a
Cara <git@caraxian.com>
2020-11-14 19:16:11 +1030

    Filterable UI Category

--- ItemSearchPlugin/Filters/ItemUICategorySearchFilter.cs
+++ ItemSearchPlugin/Filters/ItemUICategorySearchFilter.cs
@@ -3,9 +3,10 @@ using ImGuiNET;
 using Lumina.Excel.GeneratedSheets;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace ItemSearchPlugin.Filters {
-    class ItemUICategorySearchFilter : SearchFilter {
+    internal class ItemUICategorySearchFilter : SearchFilter {
         public override string Name => "Category";
         public override string NameLocalizationKey => "DalamudItemSelectCategory";
 
@@ -27,6 +28,9 @@ namespace ItemSearchPlugin.Filters {
 
         private int selectedCategory;
         private int lastCategory;
+        private string categorySearchInput = string.Empty;
+        private bool focused;
+        private readonly Vector2 popupSize = new Vector2(-1, 120);
 
         public ItemUICategorySearchFilter(ItemSearchPluginConfig config, DataManager data) : base(config) {
             uiCategories = new List<ItemUICategory> {null};
@@ -42,7 +46,47 @@ namespace ItemSearchPlugin.Filters {
 
         public override void DrawEditor() {
             ImGui.PushItemWidth(-1);
-            ImGui.Combo("##ItemUiCategorySearchFilterBox", ref this.selectedCategory, uiCategoriesArray, uiCategories.Count);
+            if (ImGui.BeginCombo("##ItemUiCategorySearchFilterBox", uiCategoriesArray[this.selectedCategory])) {
+                ImGui.SetNextItemWidth(-1);
+                ImGui.InputTextWithHint("###ItemUiCategorySearchFilterFilter", "Filter", ref categorySearchInput,  60);
+                var isFocused = ImGui.IsItemActive();
+                if (!focused) {
+                    ImGui.SetKeyboardFocusHere();
+                }
+
+                ImGui.BeginChild("###ItemUiCategorySearchFilterDisplay", popupSize, true);
+
+                if (!focused) {
+                    ImGui.SetScrollY(0);
+                    focused = true;
+                }
+
+                var c = 0;
+                var l = 0;
+                for (var i = 0; i < uiCategoriesArray.Length; i++) {
+                    if (i > 0 && categorySearchInput.Length > 0 && !uiCategoriesArray[i].ToLowerInvariant().Contains(categorySearchInput.ToLowerInvariant())) continue;
+                    if (i != 0) {
+                        c++;
+                        l = i;
+                    }
+                    if (!ImGui.Selectable(uiCategoriesArray[i], selectedCategory == i)) continue;
+                    selectedCategory = i;
+                    
+                    ImGui.CloseCurrentPopup();
+                }
+
+                ImGui.EndChild();
+                if (!isFocused && c <= 1) {
+                    selectedCategory = l;
+                    ImGui.CloseCurrentPopup();
+                }
+                
+                ImGui.EndCombo();
+            } else if (focused) {
+                focused = false;
+                categorySearchInput = string.Empty;
+            }
+            
             ImGui.PopItemWidth();
         }
 
